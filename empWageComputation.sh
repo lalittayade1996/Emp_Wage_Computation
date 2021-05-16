154d4d0b064800e582f7043df0080014d45fe462
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 22:20:07 +0530

    Added: UC3-Third-Computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,13 @@
 echo "Hello Welcome to EmpDeatils Programe"
 wagePerHr=20
 fullDay=8
+parttimehr=8
 checkRandom=$(( RANDOM%2 ))
 if [ $checkRandom -eq 1 ]
 then
 echo "Emp is Present"
 dailywage=$(( $wagePerHr*$fullDay ))
+parttimewage=$(( $wagePerHr*$parttimehr))
 else
 echo "Emp is Absent"
 fi
