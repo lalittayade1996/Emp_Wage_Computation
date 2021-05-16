85e1b8df90ded634d7b82ac74a50250756341720
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 22:30:25 +0530

    Added: UC5-Fifth-Computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,6 +2,7 @@
 echo "Hello Welcome to EmpDeatils Programe"
 wagePerHr=20
 fullDay=8
+monthdays=20
 parttimehr=8
 partfullcheck=$(( RANDOM%2 ))
 checkRandom=$(( RANDOM%2 ))
@@ -11,9 +12,11 @@ echo "Emp is Present"
 case $partfullcheck in
 0)
 dailywage=$(( $wagePerHr*$fullDay ))
+monthwage=$(( $dailywage*$monthdays))
 ;;
 1)
 parttimewage=$(( $wagePerHr*$parttimehr))
+monthwage=$(( $dailywage*$monthdays))
 ;;
 esac
 else
