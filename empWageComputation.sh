b0c0ae6eeae1bf4e76b994820471eca51e826a1c
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 22:16:43 +0530

    Added: UC2-Second-Computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -x
-
-empcheck=$((RANDOM%2))
-ispresent=1
-
-if [ $empCheck -eq $isPresent ]
+#!/bin/bash
+echo "Hello Welcome to EmpDeatils Programe"
+wagePerHr=20
+fullDay=8
+checkRandom=$(( RANDOM%2 ))
+if [ $checkRandom -eq 1 ]
 then
-	echo Employee is Present
+echo "Emp is Present"
+dailywage=$(( $wagePerHr*$fullDay ))
 else
-	echo Employee is Absent
+echo "Emp is Absent"
 fi
