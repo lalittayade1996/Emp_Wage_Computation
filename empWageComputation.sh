631825ba2f5defe519e70d49910de6fc5daff701
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 22:24:06 +0530

    Added: UC4-Fourth-Computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,12 +3,19 @@ echo "Hello Welcome to EmpDeatils Programe"
 wagePerHr=20
 fullDay=8
 parttimehr=8
+partfullcheck=$(( RANDOM%2 ))
 checkRandom=$(( RANDOM%2 ))
 if [ $checkRandom -eq 1 ]
 then
 echo "Emp is Present"
+case $partfullcheck in
+0)
 dailywage=$(( $wagePerHr*$fullDay ))
+;;
+1)
 parttimewage=$(( $wagePerHr*$parttimehr))
+;;
+esac
 else
 echo "Emp is Absent"
 fi
