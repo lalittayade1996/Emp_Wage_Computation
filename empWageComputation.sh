0c3ad09b5f78b36fe9a08e0e26c92a47d065836a
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 22:37:56 +0530

    Added: UC6-Sixth-Computation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
+
 echo "Hello Welcome to EmpDeatils Programe"
 wagePerHr=20
 fullDay=8
 monthdays=20
 parttimehr=8
+workinghrs=100
 partfullcheck=$(( RANDOM%2 ))
 checkRandom=$(( RANDOM%2 ))
 if [ $checkRandom -eq 1 ]
 then
 echo "Emp is Present"
+i=0
 case $partfullcheck in
 0)
-dailywage=$(( $wagePerHr*$fullDay ))
-monthwage=$(( $dailywage*$monthdays))
+for (( i=1; i<=$workinghrs; i++ ))
+do 
+Days=$(( $workinghrs/$monthdays ))
+dailywage=$(( $wagePerHr*$i*$Day ))
+done
 ;;
 1)
-parttimewage=$(( $wagePerHr*$parttimehr))
-monthwage=$(( $dailywage*$monthdays))
+for (( i=1; i<=$workinghrs; i++ ))
+do
+Days=$(( $workinghrs/$monthdays ))
+dailywage=$(( $wagePerHr*$i*$Day ))
+done
 ;;
 esac
 else
